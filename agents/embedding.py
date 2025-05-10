8d8d9ea6ec5a6bd5825833e3d356e1132b6f8e52
maher <31416194+maherahmed@users.noreply.github.com>
2025-05-10 16:47:08 +0200

    adding query function

--- agents/embedding.py
+++ agents/embedding.py
@@ -1,7 +1,10 @@
 # agents/embedding.py
 
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 import numpy as np
+embeddings =""
+model =""
+convert_to_numpy: bool = True
 
 def embed_documents(
     documents: list,
@@ -27,4 +30,24 @@ def embed_documents(
         doc.meta["embedding"] = emb.tolist() if hasattr(emb, "tolist") else emb
 
     print(f"✅ Embedded {len(documents)} documents with model '{model_name}'")
+    run_Querey(documents, embeddings)
     return documents
+
+
+def run_Querey(docs, embeddings1):
+    print(f"✅ Querey {len(docs)} documents with model ")
+
+    query = "who are the attendies?"
+    model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+
+    # Convert to embeddings
+    query_embedding = model.encode(query, convert_to_numpy=convert_to_numpy)
+    doc_embeddings = embeddings1
+
+    # Compute cosine similarities
+    scores = util.cos_sim(query_embedding, doc_embeddings)
+
+    # Rank documents
+    ranking = sorted(zip(scores[0], docs), key=lambda x: x[0], reverse=True)
+    for score, doc in ranking:
+        print(f"Score: {score:.4f} | Document: {doc.content}")
